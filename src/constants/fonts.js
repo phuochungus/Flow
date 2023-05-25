b5096412fbcd527176f97870bcc4dc977cf9d7e9
nyb1711 <nyb1711@gmail.com>
2023-05-26 01:47:07 +0700

    upload resource

---
+++ src/constants/fonts.js
@@ -0,0 +1,26 @@
+const NotoSans = 'NotoSans-';
+
+const FONTS= {
+    NotoSans: {
+        Black: NotoSans + 'Black',
+        BlackItalic: NotoSans + 'BlackItalic',
+        Bold: NotoSans + 'Bold',
+        BoldItalic: NotoSans + 'BoldItalic',
+        Medium: NotoSans + 'Medium',
+        MediumItalic: NotoSans + 'MediumItalic',
+        Regular: NotoSans + 'Regular',
+        ExtraBold: NotoSans + 'ExtraBold',
+        ExtraBoldItalic: NotoSans + 'ExtraBoldItalic',
+        ExtraLight: NotoSans + 'ExtraLight',
+        ExtraLightItalic: NotoSans + 'ExtraLightItalic',
+        Italic: NotoSans + 'Italic',
+        Light: NotoSans + 'Light',
+        LightItalic: NotoSans + 'LightItalic',
+        Semibold: NotoSans + 'SemiBold',
+        SemiboldItalic: NotoSans + 'SemiboldItalic',
+        Thin: NotoSans + 'Thin',
+        ThinItalic: NotoSans + 'ThinItalic',
+  }
+};
+
+export default FONTS;
