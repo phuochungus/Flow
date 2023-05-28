c18b7c7fa93375bced2ab0a37264e1ef41f7be6e
nyb1711 <nyb1711@gmail.com>
2023-05-28 18:04:32 +0700

    push screen playing

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 //import liraries
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import FONTS from './src/constants/fonts';
+import BackHeader from './src/components/back-header';
+import { IMG_BackDown } from './src/assets/images';
+import Playing from './src/screen/Playing/Playing';
 
-// create a component
 const App = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={{ width: 'auto', height: 50, fontSize: 16, color: 'black', fontFamily: FONTS.NotoSans.Bold }}>Flow App Demo</Text>
+      <Playing/>
     </View>
   )
 };
