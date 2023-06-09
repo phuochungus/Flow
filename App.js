943ea1408d599d8c026d5fde732d968ecaa1d4bd
nyb1711 <nyb1711@gmail.com>
2023-06-10 00:41:28 +0700

    restore App.js

--- App.js
+++ App.js
@@ -3,8 +3,7 @@
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigation } from './src/routes';
-import { Artist } from './src/screens/Artist/Artist';
-import { Playing } from './src/screens/Playing/Playing';
+
 // create a component
 const config = {
   screens: {
@@ -19,10 +18,9 @@ const linking = {
 
 const App = () => {
   return (
-    // <NavigationContainer linking={linking}>
-    //   <RootNavigation />
-    // </NavigationContainer>
-    <Playing />
+    <NavigationContainer linking={linking}>
+      <RootNavigation />
+    </NavigationContainer>
   );
 };
 
