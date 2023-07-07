f2e0b2dee3347dcca50963b2a04d6e6fdeec960c
nyb1711 <nyb1711@gmail.com>
2023-07-07 20:49:19 +0700

    update playlistScreen

--- src/screens/Playlist/PLaylist.jsx
+++ src/screens/Playlist/PLaylist.jsx
@@ -10,44 +10,13 @@ import scale from '../../constants/responsive';
 export const Playlist = ({navigation}) => {
 
     const {player2} = useContext(PlayingContext);
-    const [playlist, setPlayList] = useState([]);
     const [listId, setListId] = useState([]);
-    var lst = [];
-
-    const getSongInfo = async (id) => {
-        const accessToken = await AsyncStorage.getItem('access_token');
-        var myHeaders = new Headers();
-        myHeaders.append('Authorization', 'Bearer ' + accessToken);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-          redirect: 'follow',
-        };
-        await fetch(
-          'https://flow-fbmj.onrender.com/tracks/track/' + id,
-          requestOptions,
-        )
-          .then(response => response.json())
-          .then(result => {
-            temp = playlist;
-            lst.push(result);
-            console.log(lst);
-          })
-          .catch(error => console.log('error', error));
-        return null;
-    };
 
     const getListId = async ()=>{
         let temp = await AsyncStorage.getItem('list-sound');
         let list = JSON.parse(temp);
         setListId(list);
     }
-
-    // useEffect(()=>{
-    //     if (playlist.length === 0) {
-    //         getPlayList();
-    //     }
-    // }, [playlist])
 
     useEffect(()=>{
         getListId();
